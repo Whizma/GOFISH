2fa8cfb30a2f88a7cc2f31422a4c93f7ac837553
Daniel Ekelund <ekelund.daniel@hotmail.com>
2024-03-20 16:43:52 +0100

    intitial game

--- app/src/main/java/com/example/gofish/FishingGame.java
+++ app/src/main/java/com/example/gofish/FishingGame.java
@@ -9,16 +9,20 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.VibrationEffect;
 import android.os.Vibrator;
 
-public class FishingGame extends AppCompatActivity {
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
-    // Interactions
+public class FishingGame extends AppCompatActivity implements SensorEventListener {
+
     private SensorManager sensorManager;
     private Sensor sensor;
-    private SensorEventListener sensorEventListener;
 
-    // feedback
+    Timer timer;
+
     private MediaPlayer castLinePlayer;
     private Vibrator vibrator;
     @Override
@@ -27,26 +31,49 @@ public class FishingGame extends AppCompatActivity {
         setContentView(R.layout.activity_fishing_game);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
 
-        castLinePlayer = MediaPlayer.create(this, R.raw.castline);
+        castLinePlayer = MediaPlayer.create(this, R.raw.fishing_splash);
 
-        sensorEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                float x = event.values[0]; // forward/backward
-                float y = event.values[1]; // up/down
-                float z = event.values[2]; // left/right
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
 
-                float acceleration = y;
-                if (acceleration > .1) {
-                    castLinePlayer.start();
-                }
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {}
-        };
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float z = event.values[0];
+
+        if (x > 8 || z > 8) {
+            castLinePlayer.start();
+            waitForFish();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    void waitForFish() {
+        Random rand = new Random();
+        int minDelay = 5000;
+        int maxDelay = 8000;
+        int delay = rand.nextInt(maxDelay - minDelay) + minDelay;
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                long[] timings = new long[] { 50, 50, 50, 50, 50, 100, 350, 25, 25, 25, 25, 200 };
+                int[] amplitudes = new int[] { 33, 51, 75, 113, 170, 255, 0, 38, 62, 100, 160, 255 };
+                int repeatIndex = 1; // Do not repeat.
+
+                vibrator.vibrate(VibrationEffect.createWaveform(timings, amplitudes, repeatIndex));
+            }
+        }, delay);
+    }
+
 }
+
