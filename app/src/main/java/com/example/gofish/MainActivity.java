29de525af275c4038110b1846893630f6a22a446
HugoLundberg <hugo.h.lundberg@gmail.com>
2024-04-18 10:22:01 +0200

    design and colours

--- app/src/main/java/com/example/gofish/MainActivity.java
+++ app/src/main/java/com/example/gofish/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Map.class));
             }
         });
+
     }
-
-
 }
